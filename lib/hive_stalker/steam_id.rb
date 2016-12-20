33069d4ede322df51b645a013116a651518a389c
Michael Senn <michael@senn.email>
2016-12-20 16:37:23 +0100

    Adds module to extract account ID from various formats of Steam IDs.

--- lib/hive_stalker/steam_id.rb
+++ lib/hive_stalker/steam_id.rb
@@ -1,7 +1,27 @@
 module HiveStalker
-  class SteamID
-    def initialize(s)
+  module SteamID
+    STEAM_ID_64_OFFSET = 61197960265728
 
+    PATTERN_STEAM_ID = /^STEAM_0:([0-9]):([0-9]+)$/
+    PATTERN_STEAM_ID_3 = /^U:([0-9]{1,2}):([0-9]+)$/
+    PATTERN_STEAM_ID_64 = /^765([0-9]+)$/
+
+    def self.from_string(s)
+      # https://developer.valvesoftware.com/wiki/SteamID#Format
+      PATTERN_STEAM_ID.match(s) do |m|
+        return m[1].to_i + m[2].to_i * 2
+      end
+
+      PATTERN_STEAM_ID_3.match(s) do |m|
+        return m[2].to_i
+      end
+
+      PATTERN_STEAM_ID_64.match(s) do |m|
+        return m[1].to_i - STEAM_ID_64_OFFSET
+      end
+
+      # If we get until here, we did not match any regex.
+      raise ArgumentError, "#{ s.inspect } is not a supported SteamID."
     end
   end
 end
--- spec/hive-stalker/steam_id_spec.rb
+++ spec/hive-stalker/steam_id_spec.rb
@@ -1,2 +1,28 @@
 module HiveStalker
+  RSpec.describe SteamID do
+    STEAM_ID = 'STEAM_0:0:24110655'
+    STEAM_ID_3 = 'U:1:48221310'
+    STEAM_ID_64 = '76561198008487038'
+
+    describe '::from_string' do
+      it 'supports SteamID as input' do
+        steam_id = SteamID.from_string(STEAM_ID)
+        expect(steam_id).to eq 48221310
+      end
+
+      it 'supports SteamID 3 as input' do
+        steam_id = SteamID.from_string(STEAM_ID_3)
+        expect(steam_id).to eq 48221310
+      end
+
+      it 'supports SteamID 64 as input' do
+        steam_id = SteamID.from_string(STEAM_ID_64)
+        expect(steam_id).to eq 48221310
+      end
+
+      it 'raises an exception upon invalid input' do
+        expect { SteamID.from_string('foobar') }.to raise_error(ArgumentError)
+      end
+    end
+  end
 end
